7819232de3f8be5812f5181f8e0e5b88281fdcd5
SebasOE <sebastian_ortega@ceroca.cat>
2026-04-30 10:48:21 +0200

    Comienzo

---
+++ lib/componentes/cabezera.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Cabezera extends StatelessWidget {
+  const Cabezera({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
