fd4d7d1ed8e374fcf4dd36d9fed52b6675b1ade2
AlvaSPRA <alvaro_salvatierra@ceroca.cat>
2026-05-11 11:16:16 +0200

    body

---
+++ lib/componentes/apartat.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Apartat extends StatelessWidget {
+  final String url;
+  final String title;
+  const Apartat({super.key, required this.url, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width * 0.22,
+          height: 400,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 255, 255, 255),
+          ),
+
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // MediaQuery.of(context).size.width * 0.2
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 12),
+
+              Image.network(
+                url,
+                width: MediaQuery.of(context).size.width * 0.2,
+                height: MediaQuery.of(context).size.width * 0.17,
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon/componentes/apartat.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,13 +10,80 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Column(
-          
-          children: [
-            Text('Hello World!'), nigga
-          ],
+        backgroundColor: const Color.fromARGB(255, 219, 218, 218),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Image.network(
+                    'https://m.media-amazon.com/images/I/614-NN-pneL._SX3000_.jpg',
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width * 0.4,
+                    fit: BoxFit.fill,
+                  ),
+                
+                  
+                  
+                  Column(
+                    children: [
+                      const SizedBox(height: 400),
+
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        crossAxisCount: 4,
+                        children: [
+                          Apartat(
+                            url:
+                                'https://images-eu.ssl-images-amazon.com/images/G/30/EU_Stores/xPF/2026/April/Home_Garden_week/EU10/ES/Exp_HP_Single_Image_Card_DT_02_379x304_25kb._SY304_CB782725157_.jpg',
+                            title: 'Ofertas en Hogar y jardín por menos de 50€',
+                          ),
+                          Apartat(
+                            url:
+                                'https://images-eu.ssl-images-amazon.com/images/G/30/x-Site/2021/February/FujiDashPC1x._SY304_CB659988641_.jpg',
+                            title: 'Informática y accesorios',
+                          ),
+                          Apartat(
+                            url:
+                                'https://images-eu.ssl-images-amazon.com/images/G/30/x-Site/2021/February/GWDesktopSingleImageCardfitathome1x._SY304_CB659988641_.jpg',
+                            title: 'Ponte en forma desde casa',
+                          ),
+                          Apartat(
+                            url:
+                                'https://images-eu.ssl-images-amazon.com/images/G/30/EU_Stores/xPF/2026/April/Home_Garden_week/EU10/ES/Exp_HP_Single_Image_Card_DT_03_379x304_25kb._SY304_CB782725157_.jpg',
+                            title: 'Imprescindibles para el día de partido',
+                          ),
+                          Apartat(
+                            url:
+                                'https://images-eu.ssl-images-amazon.com/images/G/30/x-Site/2021/February/FujiDashPDNonprime1x._SY304_CB659988641_.jpg',
+                            title: 'Descubre los productos más vendidos',
+                          ),
+                          Apartat(
+                            url:
+                                'https://m.media-amazon.com/images/I/41fC0USPKYL._AC_SY145_.jpg',
+                            title: 'Ahorra en dispositivos Amazon',
+                          ),
+                          Apartat(
+                            url:
+                                'https://images-eu.ssl-images-amazon.com/images/G/30/EU_Stores/xPF/2026/April/Home_Garden_week/EU10/ES/Exp_HP_Single_Image_Card_DT_01_379x304_25kb._SY304_CB782725157_.jpg',
+                            title: 'Ahorra en productos con +4 estrellas',
+                          ),
+                          Apartat(
+                            url:
+                                'https://m.media-amazon.com/images/I/81lATiTUT8L._AC_SX679_.jpg',
+                            title: 'Hogar y Cocina',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
